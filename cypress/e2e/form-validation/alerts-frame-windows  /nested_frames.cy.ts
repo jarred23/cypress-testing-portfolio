83d21750f87b5cd9de6ae568b3de2ac10179cad0
“JarredRheeder” <jarredrheeder@gmail.com>
2025-08-07 15:18:53 +0200

    Clean up tracked files ignored in .gitignore

--- cypress/e2e/form-validation/alerts-frame-windows  /nested_frames.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Nested Frames Tests', () => {
-    beforeEach(() => {
-        cy.visit('https://demoqa.com/nestedframes');
-    });
-
-    it('clicks Nested Frames and verifies outer frame exists', () => {
-        cy.get('iframe#frame1').should('exist');
-    });
-});
